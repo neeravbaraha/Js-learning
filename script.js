43b3473c944685792e717a1fce831217933bb6ff
neeravbaraha <136137285+neeravbaraha@users.noreply.github.com>
2024-10-01 16:53:20 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let guessNum=43;
+let userNum=prompt("Enter the num");
+while(guessNum!=userNum){
+   let userNum=prompt("Enter the num"); 
+   
+}
+console.log("Congratulations madarchot pata lagaliyaa tuney")
