bf64c28ebb67f9080ea944e13a387a414baade5d
neeravbaraha <neeravbaraha07@gmail.com>
2024-10-09 22:50:38 +0545

    Datatype

---
+++ datatype.js
@@ -0,0 +1,11 @@
+/* 
+
+DataTypes: DataTypes are the type of data whether they are the number or string or a integer;
+here in JavaScript we dont need to defined any kind of datatype like we do in C/C++/C#;
+
+Some DataTypes are:- 
+String: something With in double quoation or single quoation the value automatically considered as string;
+example
+
+let code="JavaScript"; //Here JavaScript is a string;
+*/
