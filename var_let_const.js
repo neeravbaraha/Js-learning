692f1087f784ff7f60c848d804f3621db8dc4567
neeravbaraha <neeravbaraha07@gmail.com>
2024-10-09 22:45:07 +0545

    Var const and let updated

---
+++ var_let_const.js
@@ -0,0 +1,10 @@
+/* 
+
+
+var=Variable that can be reduced and updated;(This is not a block scope);
+let:Variable that can't be re-decleared but can be updated;(This is the block scope);
+const:Variable cannot be re-decleared or updated (This is also block scope);
+
+Block scope: Those scope that can be access with in a limited block of code these kind of variable can't be accessed anywhere;
+
+*/
