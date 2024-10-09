bc341e411092bf5e0934996798e48e76a8cce37c
neeravbaraha <neeravbaraha07@gmail.com>
2024-10-09 22:28:52 +0545

    Simple Notes are Uploaded

--- script.js
+++
@@ -1,7 +0,0 @@
-let guessNum=43;
-let userNum=prompt("Enter the num");
-while(guessNum!=userNum){
-   let userNum=prompt("Enter the num"); 
-   
-}
-console.log("Congratulations madarchot pata lagaliyaa tuney");
